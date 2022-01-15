41b0537ce392768f98d676c8175e16d2e6068288
danam <dan.am5555555@gmail.com>
2022-01-15 23:19:27 +0200

    initial

--- src/ImageHandler/HoleFiller/ImageOutput.java
+++ src/ImageHandler/HoleFiller/ImageOutput.java
@@ -10,7 +10,7 @@ public class ImageOutput {
 
     public BufferedImage setImage(Pixel[][] pixels){
         BufferedImage image = new BufferedImage(pixels.length, pixels[0].length, )//grayscale or RGB?
-        new 
+        new
     }
 
 }
